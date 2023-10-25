b37c0ff90cf93536ae4a75f1c344d8cde973ea42
harshOpensource <harshopensource@gmail.com>
2023-10-25 12:14:56 +0530

    setup and chakra done

---
+++ chakra/ChakraProvider.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { ChakraProvider } from '@chakra-ui/react'
+import { ReactNode } from 'react'
+import { theme } from './theme'
+
+const Providers = ({ children }: { children: ReactNode }) => {
+  return (
+      <ChakraProvider resetCSS theme={theme}>
+        {children}
+      </ChakraProvider>
+  )
+}
+
+export default Providers
